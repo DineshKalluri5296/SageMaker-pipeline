cc734fc776c8155176d3b68938bc9418bb2f41ca
DineshKalluri5296 <dinesh.kalluri@outlook.com>
2026-04-22 11:01:56 +0530

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -2,9 +2,13 @@ import joblib
 import pandas as pd
 from sklearn.metrics import r2_score
 
+# Load model
 model = joblib.load("model.pkl")
 
+# Load dataset (same file downloaded in train.py)
 data = pd.read_csv("data.csv")
+
+data = data.drop(["Address"], axis=1)
 
 X = data.drop("Price", axis=1)
 y = data["Price"]
